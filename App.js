732ed2cb25893a3e98a7930e8c74c360f5a56b0d
ajay <aspatil99451@gmail.com>
2023-06-30 16:14:33 +0530

    Text box and Heading design

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TextInput } from 'react-native';
+import {textFormatting, textBoxFormatting} from "./designConfig/designFormatters";
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <SafeAreaView>
+        <View style={styles.container}>
+          <Text style={textFormatting.title}>PodAI</Text>
+            <StatusBar style="auto" />
+
+            <View style={styles.textViewContainer}>
+                <TextInput style={textBoxFormatting.textBox} placeholder="Enter your prompt here"/>
+            </View>
+
+        </View>
+      </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+      marginLeft: 20,
+  },
+  textViewContainer: {
+      height: "45%",
+      width: "85%",
+      flexDirection: 'row',
+      marginTop: '25%',
+      backgroundColor: '#2dbe7d',
   },
 });
+
+
---
+++ designConfig/designFormatters.js
@@ -0,0 +1,18 @@
+import {StyleSheet} from "react-native";
+
+export const textFormatting = StyleSheet.create({
+    title: {
+        fontSize: 30,
+        fontFamily: 'Gill Sans',
+        marginTop: 20,
+    },
+});
+
+export const textBoxFormatting = StyleSheet.create({
+    textBox: {
+        height: "100%",
+        width: "100%",
+        borderColor: 'gray',
+        borderWidth: 1,
+    }
+});
