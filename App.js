326d58b0625ebe74f32e4c54ac94e980f66c438f
ajay <aspatil99451@gmail.com>
2023-06-30 18:48:10 +0530

    Textbox design updates. Send icon updated. TextBox working when send button is pressed

--- App.js
+++ App.js
@@ -1,36 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, TextInput } from 'react-native';
-import {textFormatting, textBoxFormatting} from "./designConfig/designFormatters";
-
+import HomeView from "./Screens/homeScreen";
+import {SafeAreaView, StyleSheet, View} from "react-native";
 
 export default function App() {
   return (
-      <SafeAreaView>
-        <View style={styles.container}>
-          <Text style={textFormatting.title}>PodAI</Text>
-            <StatusBar style="auto" />
 
-            <View style={styles.textViewContainer}>
-                <TextInput style={textBoxFormatting.textBox} placeholder="Enter your prompt here"/>
-            </View>
+        <View style={styles.container}>
+
+            <SafeAreaView style={styles.safeAreaStyle}>
+            <HomeView/>
+            </SafeAreaView>
 
         </View>
-      </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff',
-      marginLeft: 20,
-  },
-  textViewContainer: {
-      height: "45%",
-      width: "85%",
-      flexDirection: 'row',
-      marginTop: '25%',
-      backgroundColor: '#2dbe7d',
-  },
+ const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#f9f6f6',
+        height: "100%",
+        width: "100%",
+    },
+     safeAreaStyle: {
+         marginLeft: 20,
+         marginRight: 20,
+     }
 });
-
 
--- designConfig/designFormatters.js
+++ designConfig/designFormatters.js
@@ -2,17 +2,44 @@ import {StyleSheet} from "react-native";
 
 export const textFormatting = StyleSheet.create({
     title: {
-        fontSize: 30,
+        fontSize: 35,
         fontFamily: 'Gill Sans',
         marginTop: 20,
+    },
+    fieldTitle: {
+        fontSize: 20,
+        fontFamily: 'Gill Sans',
+        marginTop: 10,
+        color: '#2dbe7d',
+        marginBottom: 20,
     },
 });
 
 export const textBoxFormatting = StyleSheet.create({
     textBox: {
-        height: "100%",
+        height: "60%",
         width: "100%",
-        borderColor: 'gray',
-        borderWidth: 1,
+        borderColor: 'green',
+        borderRadius: 5,
+        backgroundColor: '#fff',
+    },
+    textInputStyle: {
+        marginTop: 10,
+        marginLeft: 10,
+        height: '100%',
+        fontSize: 18,
+        fontFamily: 'Gill Sans',
+        fontStyle: 'italic',
     }
+});
+
+export const viewContainersFormatting = StyleSheet.create({
+        textViewContainer: {
+        height: "60%",
+        width: "auto",
+        flexDirection: 'column',
+        marginTop: '8%',
+        // backgroundColor: '#2dbe7d',
+
+    },
 });
